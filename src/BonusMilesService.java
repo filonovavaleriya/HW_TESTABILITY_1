5613f36279f0cdeb6d74b45b8598f3f070be99df
Filonova Valeriya <filonova_valeriya@bk.ru>
2026-04-07 21:36:22 +0300

    The answer to the task_2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class BonusMilesService {
+
+    public int calculate(int ticketPrice) {
+        int result = (ticketPrice / 20);
+        return result;
+
+    }
 }
